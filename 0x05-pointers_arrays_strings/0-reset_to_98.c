ba473b1a8ff015f6aba9dd2287833e8b1ddba576
salmehd <hormortorshor1@gmail.com>
2023-03-28 01:13:39 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer  98
+ * @n:- the value to be updated to 98
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
