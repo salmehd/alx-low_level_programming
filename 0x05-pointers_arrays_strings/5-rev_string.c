218f55dc0bc73944c2ed176bce02b73dde3749b5
salmehd <hormortorshor1@gmail.com>
2023-03-29 10:19:34 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,15 @@ void rev_string(char *s)
 	int length, z, half;
 	char temp;
 
-	for (length = 0; s[length] != '\0'; length++);
+	for (length = 0; s[length] != '\0'; length++)
+	;
 	z = 0;
 	half = length / 2;
 
 	while (half--)
 	{
 		temp = s[length - z - 1];
+		s[length - z - 1] = s[z];
 		s[z] = temp;
 		z++;
 	}
