05cb1d5c970f4647f49d64702e651606ee903d47
salmehd <hormortorshor1@gmail.com>
2023-03-31 15:48:13 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,36 @@
 #include "main.h"
 /**
-* leet - encode into 1337peak
-* @n: input value
-* Return: n value
-*/
-
-char *leet(char *n)
+ *leet - encodes a string into 1337.
+ *letters a and A are replaced by 4.
+ *Letters e and E are replaced by 3
+ *Letters o and O are replaced by 0
+ *Letters t and T are replaced by 7
+ *Letters l and L are replaced by 1
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
+ */
+char *leet(char *s)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTIL";
-	char s2[] = "4433007711";
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
 
-	for (i  = 0; n[j] != '\0'; i++)
+/*  scan through string */
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+/* check whether leetLetter is found */
 	{
-		for (j = 0; j < 10; j++)
+		leetCount = 0;
+		while (leetCount < 10)
 		{
-			if (n[i] == s1[j])
+			if (leetLetters[leetCount] == s[stringCount])
 			{
-				n[i] = s2[j];
+				s[stringCount] = leetNums[leetCount];
 			}
+			leetCount++;
 		}
+		stringCount++;
 	}
-	return (n);
-
+	return (s);
 }
-
