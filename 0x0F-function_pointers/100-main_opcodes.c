58842b6bc9b1e2ae49b298cf7bef298037d17605
salmehd <hormortorshor1@gmail.com>
2023-04-23 21:44:19 -0700

    f_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,37 +10,20 @@
 
 int main(int argc, char *argv[])
 {
-	int bytes, index;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
+	int j;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = *(unsigned char *)address;
-		printf("%.2x", opcode);
-
-		if (index == bytes - 1)
-			continue;
-		printf(" ");
-
-		address++;
-	}
-
-	printf("\n");
-
+	for (j = 0; j < atoi(argv[1]) - 1; j++)
+		printf("%02hhx ", ((char *)main)[j]);
+	printf("%02hhx\n", ((char *)main)[j]);
 	return (0);
 }
